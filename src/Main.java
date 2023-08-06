4d943e4e7a52b6493ca1047fbe216f544ece446f
Mekentosh <m87022928044@gmail.com>
2023-08-07 02:26:43 +0600

    13

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,44 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        BST<Integer, String> bst = new BST<>();
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        // Test put method
+        bst.put(15, "Apple");
+        bst.put(10, "Banana");
+        bst.put(20, "Orange");
+        bst.put(7, "Pizza");
+        bst.put(13, "Burger");
+        bst.put(17, "Fries");
+        bst.put(25, "Cola");
+        bst.put(4, "Kyoto");
+        bst.put(9, "Akihabara");
+        bst.put(12, "Saitama");
+        bst.put(14, "Tokyo");
+        bst.put(16, "Japan");
+        bst.put(19, "China");
+        bst.put(21, "USA");
+        bst.put(26, "Kazakhstan");
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        // Test get method
+        System.out.println("Get 12: " + bst.get(12));
+        System.out.println("Get 7: " + bst.get(7));
+        System.out.println("Get 25: " + bst.get(25));
+        System.out.println("Get 5: " + bst.get(5));
+
+        // Test delete method
+        bst.delete(10);
+        bst.delete(21);
+
+        // Test iterator method
+        Iterable<BST.KeyValue<Integer, String>> iterator = bst.iterator();
+        for (BST.KeyValue<Integer, String> elem : iterator) {
+            System.out.println(elem.getKey() + " - " + elem.getValue());
         }
+
+        // Test size method
+        System.out.println("Size: " + bst.size());
+
+        // Test height method
+        System.out.println("Height: " + bst.height());
     }
 }
